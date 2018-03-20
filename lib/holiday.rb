6d7a66d01c9d10cd017508b5749ebd666e021191
michael hohl <mhohl@rocketmail.com>
2018-03-20 20:21:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}"
+    holiday.each do |holiday, supplies|
+      puts "#{holiday}: #{supplies.join}"
+    end
   end
 end
 
